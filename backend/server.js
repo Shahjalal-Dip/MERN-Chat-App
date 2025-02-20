1801a1ef9dbfed08bd1bbb38360ae6bb9d0445cd
Shah Jalal Dip <shahjalaldip246@gmail.com>
2025-02-20 14:01:05 +0600

    deploying

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
+import {app, server } from "./socket/socket.js";
 
 
-const app = express();
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
 
 dotenv.config();
 
@@ -20,14 +23,14 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-// app.get('/', (req, res) => {
-//   //root route http://localhost:5000/
-//   res.send('Hello World!');
-// });
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/frontend/dist/index.html"));
+});
 
 
-
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   connectToMongoDB();
   console.log(`Server is running on ${PORT}`);
 });
